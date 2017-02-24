1da6fe447586ea83ea4be8c7b1e8e6502228fc26
Artur Rabenda <smithhero1994@o2.pl>
2017-02-24 19:26:34 +0100

    miiii

--- ArturekPlik.java
+++ ArturekPlik.java
@@ -15,7 +15,7 @@ public class ArturekPlik {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("mmmm mmm mmm");
     }
     
 }
